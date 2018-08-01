4dd07290468e1426fc4a6a631f0dbd4009567a0d
Graham Spencer <grahamspence@gmail.com>
2018-08-01 17:30:31 -0400

    added favourite links

--- server.rb
+++ server.rb
@@ -9,5 +9,12 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['Git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['games', 'movies', 'music', 'coding']
   erb :about
+end
+
+get '/favourites' do
+  @fav_links = ['https://www.twitter.com', 'https://www.youtube.com', 'https://www.reddit.com', 'https://www.thestar.com', 'https://www.theonion.com']
+  erb :favourites
 end
