bf385cc8e0113d5a7e0f626e174c0a0e37e349f8
Graham Spencer <grahamspence@gmail.com>
2018-08-01 20:46:29 -0400

    exercise complete

--- server.rb
+++ server.rb
@@ -18,3 +18,11 @@ get '/favourites' do
   @fav_links = ['https://www.twitter.com', 'https://www.youtube.com', 'https://www.reddit.com', 'https://www.thestar.com', 'https://www.theonion.com']
   erb :favourites
 end
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to ('/portfolio')
+end
